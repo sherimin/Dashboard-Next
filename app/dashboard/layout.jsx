79a98a4463044d0edfc205495a2235f3c3600c34
Sheri Lu <sherisherilu@gmail.com>
2023-12-01 15:24:54 -0800

    Add footer.

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../ui/dashboard/navbar/navbar"
 import Sidebar from "../ui/dashboard/sidebar/sidebar"
 import styles from "../ui/dashboard/dashboard.module.css"
+import Footer from "../ui/dashboard/footer/footer"
 
 
 const Layout = ({ children }) => {
@@ -12,6 +13,7 @@ const Layout = ({ children }) => {
         <div className={styles.content}>
             <Navbar />
             {children}
+            <Footer />
         </div>
     </div>
   )
