a718f91f5fed1d36f7ff27a4de10400cc80f022d
Sheri Lu <sherisherilu@gmail.com>
2023-11-30 19:44:50 -0800

    Add transactions section.

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../ui/dashboard/navbar/navbar"
 import Sidebar from "../ui/dashboard/sidebar/sidebar"
 import styles from "../ui/dashboard/dashboard.module.css"
+import RightBar from "../ui/dashboard/rightbar/rightbar"
 
 const Layout = ({ children }) => {
   return (
@@ -11,6 +12,9 @@ const Layout = ({ children }) => {
         <div className={styles.contacts}>
             <Navbar />
             {children}
+        </div>
+        <div>
+          <RightBar />
         </div>
     </div>
   )
--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -1,6 +1,83 @@
+import Image from 'next/image'
+import styles from './transactions.module.css'
+
 const Transactions = () => {
   return (
-    <div>Transactions</div>
+    <div className={styles.container}>
+      <h2 className={styles.title}>Latest Transactions</h2>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Date</td>
+            <td>Amount</td>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+              <Image src="/noavatar.png" alt="" width={40} height={40} className={styles.userImage} />
+              Rachelle R.
+              </div>
+            </td>
+            
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>Pending</span>
+            </td>
+            <td>30.11.2023</td>
+            <td>$ 52.90</td>
+          </tr>
+
+          <tr>
+            <td>
+            <div className={styles.user}>
+              <Image src="/noavatar.png" alt="" width={40} height={40} className={styles.userImage} />
+              Julia C.
+            </div>
+            </td>
+            
+            <td>
+              <span className={`${styles.status} ${styles.cancelled}`}>Cancelled</span>
+            </td>
+            <td>29.11.2023</td>
+            <td>$ 102.40</td>
+          </tr>
+
+          <tr>
+            <td>
+              <div className={styles.user}>
+              <Image src="/noavatar.png" alt="" width={40} height={40} className={styles.userImage} />
+              Zach E.
+              </div>
+            </td>
+            
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>Pending</span>
+            </td>
+            <td>26.11.2023</td>
+            <td>$ 32.81</td>
+          </tr>
+
+          <tr>
+            <td>
+              <div className={styles.user}>
+              <Image src="/noavatar.png" alt="" width={40} height={40} className={styles.userImage} />
+              Katlyn T.
+              </div>
+            </td>
+            
+            <td>
+              <span className={`${styles.status} ${styles.done}`}>Done</span>
+            </td>
+            <td>20.11.2023</td>
+            <td>$ 77.28</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
