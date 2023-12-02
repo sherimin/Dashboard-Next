9c18b2edeffc3e10d707147ad570b8f65feb40b8
Sheri Lu <sherisherilu@gmail.com>
2023-12-01 19:19:10 -0800

    Add a table to display user information.

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,6 +1,59 @@
+import Search from "@/app/ui/dashboard/search/search"
+import styles from "@/app/ui/dashboard/users/users.module.css"
+import Image from "next/image"
+import Link from "next/link"
+
 const UsersPage = () => {
   return (
-    <div>UsersPage</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user" />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>
+            Add User
+          </button>
+        </Link>
+      </div>
+
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image 
+                  src="/noavatar.png" 
+                  alt="JoProfile" 
+                  width={40} 
+                  height={40} 
+                  className={styles.userImage} 
+                />
+                Josephine B.
+              </div>
+            </td>
+            <td>example@example.com</td>
+            <td>02.05.2023</td>
+            <td>Admin</td>
+            <td>Active</td>
+            <td>
+              <Link href="/">
+                <button className={`${styles.button} ${styles.view}`}>View</button>
+              </Link>
+              <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
