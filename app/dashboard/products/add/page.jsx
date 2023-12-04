e8d4888404a905d3bd329350b4be444b2ce19367
Sheri Lu <sherisherilu@gmail.com>
2023-12-03 18:55:37 -0800

    Style the add product page.

---
+++ app/dashboard/products/add/page.jsx
@@ -0,0 +1,31 @@
+import styles from "@/app/ui/dashboard/products/addProduct/addProduct.module.css"
+
+const AddProductPage = () => {
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input type="text" placeholder='title' name='title' required />
+        <select name="Category" id="category">
+          <option value="general">Choose a category</option>
+          <option value="books">Books</option>
+          <option value="cosmetics">Cosmetics</option>
+          <option value="electronics">Electronics</option>
+          <option value="fashion">Fashion</option>
+          <option value="home">Home</option>
+        </select>
+
+        <input type='number' placeholder='price' name='price' />
+        <input type='number' placeholder='stock' name='stock' />
+        <input type='text' placeholder='color' name='color' />
+        <input type='text' placeholder='size' name='size' />
+
+        <textarea name="desc" id="desc" rows="16" placeholder="Description"  />
+
+        <button type="submit">Submit</button>
+
+      </form>
+    </div>
+  )
+}
+
+export default AddProductPage
---
+++ app/dashboard/users/add/page.jsx
@@ -0,0 +1,9 @@
+import styles from "@/app/ui/dashboard/Users/addUser/addUser.module.css"
+
+const AddUserPage = () => {
+  return (
+    <div className={styles.container}>AddUserPage</div>
+  )
+}
+
+export default AddUserPage
