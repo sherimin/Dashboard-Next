4851e785917b9ae0d2cf3413e370463ceefd6d70
Sheri Lu <sherisherilu@gmail.com>
2023-12-03 19:14:24 -0800

    Style the add user page.

--- app/dashboard/users/add/page.jsx
+++ app/dashboard/users/add/page.jsx
@@ -2,7 +2,31 @@ import styles from "@/app/ui/dashboard/Users/addUser/addUser.module.css"
 
 const AddUserPage = () => {
   return (
-    <div className={styles.container}>AddUserPage</div>
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input type="text" placeholder='Username' name='Username' required />
+        <input type='email' placeholder='Email' name='Email' required />
+        <input type='password' placeholder='Password' name='Password' required />
+        <input type='phone' placeholder='Phone' name='Phone' />
+
+        <select name="isAdmin" id="isAdmin">
+          <option value={false} selected>Is Admin?</option>
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
+        </select>
+        <select name="isActive" id="isActive">
+          <option value={false} selected>Is Active?</option>
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
+        </select>
+
+        <textarea name="desc" id="desc" rows="7" placeholder="Address"  />
+
+
+        <button type="submit">Submit</button>
+
+      </form>
+    </div>
   )
 }
 
