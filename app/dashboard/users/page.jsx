e1e0f8eaf81f55ce45b262ac3e1af0cbfb5af2fb
Sheri Lu <sherisherilu@gmail.com>
2023-12-03 16:03:02 -0800

    Style the user table.

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -45,10 +45,12 @@ const UsersPage = () => {
             <td>Admin</td>
             <td>Active</td>
             <td>
-              <Link href="/">
-                <button className={`${styles.button} ${styles.view}`}>View</button>
-              </Link>
-              <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>View</button>
+                </Link>
+                  <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+              </div>
             </td>
           </tr>
         </tbody>
