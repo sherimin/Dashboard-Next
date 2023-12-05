a81982c99e31a84cc2810d4f6d39f809ba9301cd
Sheri Lu <sherisherilu@gmail.com>
2023-12-05 13:29:31 -0800

    Connect MongoDB.

---
+++ app/lib/utils.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const connectToDB = async () => {
+
+    const connection = {}
+
+    try {
+        if (connection.isConnected) return;
+        const db = await mongoose.connect(process.env.MONGO);
+        connection.isConnected = db.connection[0].readyState
+      } catch (error) {
+        handleError(error);
+      }
+}
