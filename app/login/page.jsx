19ddeb8aa56f0ecdc6d68b8ac5cd7a119a7d5f14
Sheri Lu <sherisherilu@gmail.com>
2023-12-04 17:42:27 -0800

    Add login page.

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import styles from '@/app/ui/login/login.module.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className={styles.container}>
+      <form className={styles.form} action=''>
+        <h1>Please login</h1>
+        <input type="text" placeholder='Username' />
+        <input type="password" placeholder='Password' />
+        <button>Login</button>
+      </form>
+    </div>
   )
 }
 
