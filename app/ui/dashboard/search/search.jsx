a02c0303cf26b04d1381adbaa8160c18445bcd1f
Sheri Lu <sherisherilu@gmail.com>
2023-12-06 12:48:19 -0800

    Add search function.

--- app/ui/dashboard/search/search.jsx
+++ app/ui/dashboard/search/search.jsx
@@ -1,11 +1,30 @@
+"use client"
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { MdSearch } from 'react-icons/md'
 import styles from './search.module.css'
 
 const Search = ({ placeholder }) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const handleSearch = (e) => {
+
+    const params = new URLSearchParams(searchParams);
+
+    params.set("q", e.target.value);
+
+    replace(`${pathname}?${params}`)
+  }
+
+  console.log('searchParams: ', searchParams);
+  console.log('pathname: ', pathname);
+
   return (
     <div className={styles.container}>
         <MdSearch />
-        <input type="text" placeholder={placeholder} className={styles.input} />
+        <input type="text" placeholder={placeholder} className={styles.input} onChange={handleSearch} />
     </div>
   )
 }
