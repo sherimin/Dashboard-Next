244f326e11965e83baff42a35a68885775439559
Sheri Lu <sherisherilu@gmail.com>
2023-12-12 17:00:31 -0800

    Add user authentication.

---
+++ app/authconfig.js
@@ -0,0 +1,24 @@
+const authConfig = {
+    pages:{
+        signin:"/login",
+    },
+    callbacks:{
+        authorized({auth, request}) {
+            const isLoggedIn = auth?.user;
+            const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard")
+
+            if (isOnDashboard) {
+                if (isLoggedIn) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else if (isLoggedIn) {
+                return Response.redirect(new URL("/dashboard", request.nextUrl))
+            }
+            return true;
+        }
+    }
+}
+
+export default authConfig
